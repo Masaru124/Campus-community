26ee7e2dc547f22af11254f83d3f808cf9c64a89
Masaru124 <masarukaze041@gmail.com>
2025-08-23 21:02:48 +0530

    admin

--- app/constants.py
+++ app/constants.py
@@ -1,5 +1,5 @@
 # Role constants for the campus platform
-ADMIN_ROLES = ["teacher", "hod", "club_leader"]
+ADMIN_ROLES = ["teacher", "hod", "club_leader", "admin"]
 USER_ROLES = ["student", "alumni", "faculty"]
 
 # All valid roles
